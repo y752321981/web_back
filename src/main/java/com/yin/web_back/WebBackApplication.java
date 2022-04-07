f31197321bb7eac6be072e51c4dbb8b8cdacda12
尹东川 <yinki99@qq.com>
2022-04-07 18:03:06 +0800

    初始化

---
+++ src/main/java/com/yin/web_back/WebBackApplication.java
@@ -0,0 +1,13 @@
+package com.yin.web_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yin/web_back/WebBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yin.web_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
